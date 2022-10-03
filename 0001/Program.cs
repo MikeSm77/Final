440813e2df438f2c36c998245e84ae9ad41ccb72
Your Mike <regdrug@gmail.com>
2022-10-03 20:29:18 +0300

    Add program.cs

---
+++ 0001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Укажите количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayString = new string[size];
+int A = 0;
+int B = 3;
+
+for (int i = 0; i < size; i++){
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+
+    if (element.Length <= B){
+        arrayString[A] = element;
+        A++;
